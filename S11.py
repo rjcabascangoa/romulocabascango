cd42dc4e3b052b79af4b9d2553c5cea7407a43ad
ROMULO CABASCANGO <rj.cabascangoa@uea.edu.ec>
2024-02-21 18:42:50 -0500

    deber

--- S11.py
+++ S11.py
@@ -1,7 +1,17 @@
-print("MATRIZ 3X3")
+matriz = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]
+]
 
-matriz = [[1, 2, 3],
-          [4, 5,6],
-          [7, 8, 9]]
-print(matriz)
-print("Encontrar la posicion 5",matriz[1],[1])
+no_fila = 0
+busqueda = int(input('Ingrese el valor de busqueda: '))
+
+for fila in matriz:
+ no_columna = 0
+for columna in fila:
+ if (columna == busqueda):
+     print(f'No. {busqueda}, posision es: [{no_fila}][{no_columna}]')
+     no_columna += 1
+     no_fila += 1
+
