f62107dc8ab22aef17092223fe01d4d4931220ce
ROMULO CABASCANGO <rj.cabascangoa@uea.edu.ec>
2024-03-06 14:56:44 -0500

    deber

---
+++ SEMANA 11/suma-funcion.py
@@ -0,0 +1,6 @@
+def suma(a,b):
+    return a+b
+resultado = suma(2,2)
+print(resultado)
+resultado = suma(5,2)
+print(resultado)
---
+++ SEMANA 11/tabla_funciones.py
@@ -0,0 +1,6 @@
+def tabla_funciones(n):
+    for i in range(1,11):
+        print(n,'x',i,'=',i*n)
+tabla_funciones(5)
+print("++++++++++++++++++++")
+tabla_funciones(4)
