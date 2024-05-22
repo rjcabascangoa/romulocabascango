550151fc9d04633c9ee6ea489aefa2a331d0bb5d
ROMULO CABASCANGO <rj.cabascangoa@uea.edu.ec>
2024-05-21 22:51:42 -0500

    Merge remote-tracking branch 'origin/main'

---
+++ SEMANA 11/poo/clases y objeto IV.py
@@ -0,0 +1,15 @@
+class nombre:
+    pass
+victor = nombre ()
+katty  = nombre()
+#objeto.atributob= valor
+victor.edad=30
+victor.sexo='masculino'
+victor.pais='escuador'
+
+katty.edad=32
+katty.sexo='femenino'
+katty.pais='Cayambe'
+
+print(katty.edad)
+print(victor.edad)
---
+++ SEMANA 11/poo/metodos.py
@@ -0,0 +1,1 @@
+print("Metodos")
