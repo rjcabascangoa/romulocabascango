2e1f81bc703deb37c85d8eafba554d04817a666c
ROMULO CABASCANGO <rj.cabascangoa@uea.edu.ec>
2024-05-23 20:45:29 -0500

    Merge remote-tracking branch 'origin/main'

--- SEMANA 11/poo/metodos.py
+++ SEMANA 11/poo/metodos.py
@@ -1,1 +1,8 @@
 print("Metodos")
+class Ropa:
+    def __init__(self):
+        self.marca='Luna'
+        self.colo='azul'
+        self.talla='M'
+camisa= Ropa()
+print(camisa.marca,camisa.colo,camisa.talla)
